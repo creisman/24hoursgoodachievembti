db81a279ff0061701c8fc5367e8e9aaa320a7685
conorreisman@gmail.com <conorreisman@gmail.com@75e45897-a8af-4d69-cf30-fb7e5db3fcbb>
2012-10-20 09:45:57 +0000

    Changed the select box values to single chars. Added JS validation.
    git-svn-id: http://24-hours-good-achieve-mbti.googlecode.com/svn/trunk@16 75e45897-a8af-4d69-cf30-fb7e5db3fcbb

--- war/scripts/index.js
+++ war/scripts/index.js
@@ -1,12 +1,39 @@
 window.onload = function() {
-	console.log("enter");
 	initialize();
 };
 
 function initialize() {
-	var form = document.getElementById("mbtiform");
-	form.addEventListener("submit", function() {
-		console.log("test");
-		return false;
+	// Add form validation.
+	var form = $("#mbtiform")[0];
+	form.addEventListener("submit", function(e) {
+		// If it doesn't validate then don't submit.
+		console.log(validate());
+		if(!validate()) {
+			e.preventDefault();
+		}
+		//TODO this needs an error message.
 	});
 };
+
+function validate() {
+	var valid = true;
+	var elements = $(".percentbox");
+	elements.each(function(i, element) {
+		var val = parseInt(element.value);
+		if (isNaN(val) || element.value < 0 || element.value > 100) {
+			$(element).addClass("error");
+			valid = false;
+		}
+	});
+
+	elements = $(".typeselect");
+	elements.each(function(i, element) {
+		console.log(element.value);
+		if (element.value == "") {
+			$(element).addClass("error");
+			valid = false;
+		}
+	});
+
+	return valid;
+}
